b46b918b52655ee1a971bae058e6730e58ab8d4e
kato yuko <yy_yuko_121@yahoo.co.jp>
2020-02-11 14:48:13 +0900

    rails g で不要ファイルが生成されない記述追加

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Sample
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # この二行の記述で自動生成しない設定を作成しています。
+    g.assets false
+    g.helper false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_013108) do
+ActiveRecord::Schema.define(version: 2019_12_28_071523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2019_11_06_013108) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
+  end
+
+  create_table "feeds", force: :cascade do |t|
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "inquiries", force: :cascade do |t|
